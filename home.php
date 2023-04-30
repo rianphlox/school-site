4de29f53b68ebdf6f37af2e36b1f36a8e23ac1d5
rianphlox <skellybrownston@gmail.com>
2023-05-01 00:08:11 +0100

    commit

--- home.php
+++ home.php
@@ -1,82 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include 'inc/home_head.php' ?>
 
-<head>
-    <title>Fox University</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="stylesheet" href="css/ionicons.min.css">
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
-    
-</head>
 
 <body>
-    <div class="bg-top navbar-light">
-        <div class="container">
-            <div class="row no-gutters d-flex align-items-center align-items-stretch">
-                <div class="col-md-4 d-flex align-items-center py-4">
-                    <a class="navbar-brand" href="home">Fox. <span>University</span></a>
-                </div>
-                <div class="col-lg-8 d-block">
-                    <div class="row d-flex">
-                        <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
-                            <div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
-                            <div class="text">
-                                <span>Email</span>
-                                <span><a href="#" class="__cf_email__" data-cfemail="e990869c9b8c84888085a98c84888085c78a8684">[email&#160;protected]</a></span>
-                            </div>
-                        </div>
-                        <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
-                            <div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
-                            <div class="text">
-                                <span>Call</span>
-                                <span>Call Us: + 1235 2355 98</span>
-                            </div>
-                        </div>
-                        <div class="col-md topper d-flex align-items-center justify-content-end">
-                            <p class="mb-0">
-                                <a href="#" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
-                                    <span>Apply now</span>
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
-        <div class="container d-flex align-items-center px-4">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="oi oi-menu"></span> Menu
-            </button>
-            <form action="#" class="searchform order-lg-last">
-                <div class="form-group d-flex">
-                    <input type="text" class="form-control pl-3" placeholder="Search">
-                    <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
-                </div>
-            </form>
-            <div class="collapse navbar-collapse" id="ftco-nav">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"><a href="index" class="nav-link pl-0">Home</a></li>
-                    <li class="nav-item"><a href="about" class="nav-link">About</a></li>
-                    <li class="nav-item"><a href="courses" class="nav-link">Courses</a></li>
-                    <li class="nav-item"><a href="staff" class="nav-link">Staff</a></li>
-                    <li class="nav-item"><a href="blog" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="contact" class="nav-link">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include 'inc/nav.php' ?>
 
     <section class="home-slider owl-carousel">
         <div class="slider-item" style="background-image:url(images/bg_1.jpg);">
@@ -772,7 +698,7 @@
                         Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
                         <script>
                             document.write(new Date().getFullYear());
-                        </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="#" target="_blank">Colorlib</a>
                     </p>
                 </div>
             </div>
---
+++ inc/sidebar.php
@@ -0,0 +1,140 @@
+<div class="sidebar" id="sidebar">
+            <div class="sidebar-inner slimscroll">
+                <div id="sidebar-menu" class="sidebar-menu">
+                    <ul>
+                        <li class="menu-title">
+                            <span>Main Menu</span>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="feather-grid"></i> <span> Dashboard</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="index">Admin Dashboard</a></li>
+                                <li><a href="teacher-dashboard">Teacher Dashboard</a></li>
+                                <li><a href="student-dashboard">Student Dashboard</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu active">
+                            <a href="#"><i class="fas fa-graduation-cap"></i> <span> Students</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <!-- <li><a href="students" class="active">Student List</a></li> -->
+                                <li><a href="student-details">Student View</a></li>
+                                <!-- <li><a href="add-student">Student Add</a></li> -->
+                                <li><a href="edit-student">Student Edit</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-chalkboard-teacher"></i> <span> Teachers</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="teachers">Teacher List</a></li>
+                                <li><a href="teacher-details">Teacher View</a></li>
+                                <li><a href="add-teacher">Teacher Add</a></li>
+                                <li><a href="edit-teacher">Teacher Edit</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-building"></i> <span> Departments</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="departments">Department List</a></li>
+                                <li><a href="add-department">Department Add</a></li>
+                                <li><a href="edit-department">Department Edit</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-book-reader"></i> <span> Subjects</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="subjects">Subject List</a></li>
+                                <li><a href="add-subject">Subject Add</a></li>
+                                <li><a href="edit-subject">Subject Edit</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-clipboard"></i> <span> Invoices</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="invoices">Invoices List</a></li>
+                                <li><a href="invoice-grid">Invoices Grid</a></li>
+                                <li><a href="add-invoice">Add Invoices</a></li>
+                                <li><a href="edit-invoice">Edit Invoices</a></li>
+                                <li><a href="view-invoice">Invoices Details</a></li>
+                                <li><a href="invoices-settings">Invoices Settings</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-title">
+                            <span>Management</span>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-file-invoice-dollar"></i> <span> Accounts</span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="fees-collections">Fees Collection</a></li>
+                                <li><a href="expenses">Expenses</a></li>
+                                <li><a href="salary">Salary</a></li>
+                                <li><a href="add-fees-collection">Add Fees</a></li>
+                                <li><a href="add-expenses">Add Expenses</a></li>
+                                <li><a href="add-salary">Add Salary</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="holiday"><i class="fas fa-holly-berry"></i> <span>Holiday</span></a>
+                        </li>
+                        <li>
+                            <a href="fees"><i class="fas fa-comment-dollar"></i> <span>Fees</span></a>
+                        </li>
+                        <li>
+                            <a href="exam"><i class="fas fa-clipboard-list"></i> <span>Exam list</span></a>
+                        </li>
+                        <li>
+                            <a href="event"><i class="fas fa-calendar-day"></i> <span>Events</span></a>
+                        </li>
+                        <li>
+                            <a href="time-table"><i class="fas fa-table"></i> <span>Time Table</span></a>
+                        </li>
+                        <li>
+                            <a href="library"><i class="fas fa-book"></i> <span>Library</span></a>
+                        </li>
+                        <li>
+                            <a href="settings"><i class="fas fa-cog"></i> <span>Settings</span></a>
+                        </li>
+                        <li class="menu-title">
+                            <span>Pages</span>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-shield-alt"></i> <span> Authentication </span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="login">Login</a></li>
+                                <li><a href="register">Register</a></li>
+                                <li><a href="forgot-password">Forgot Password</a></li>
+                                <li><a href="error-404">Error Page</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="blank-page"><i class="fas fa-file"></i> <span>Blank Page</span></a>
+                        </li>
+                        <li class="menu-title">
+                            <span>Others</span>
+                        </li>
+                        <li>
+                            <a href="sports"><i class="fas fa-baseball-ball"></i> <span>Sports</span></a>
+                        </li>
+                        <li>
+                            <a href="hostel"><i class="fas fa-hotel"></i> <span>Hostel</span></a>
+                        </li>
+                        <li>
+                            <a href="transport"><i class="fas fa-bus"></i> <span>Transport</span></a>
+                        </li>
+                        <li class="menu-title">
+                            <span>UI Interface</span>
+                        </li>
+                        <li class="submenu">
+                            <a href="#"><i class="fas fa-table"></i> <span> Tables </span> <span class="menu-arrow"></span></a>
+                            <ul>
+                                <li><a href="tables-basic">Basic Tables </a></li>
+                                <li><a href="data-tables">Data Table </a></li>
+                            </ul>
+                        </li>
+                        
+                        
+                        
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
